89848d04936e22e4a2f413db20b95334413ec571
jphillips <josh.phillips@fearnworks.com>
2023-09-16 06:47:08 -0500

    Add test requirements install

--- scripts/test_client.sh
+++ scripts/test_client.sh
@@ -1,7 +1,7 @@
 source venv/bin/activate
 cd ./m365client
 pip install -r requirements/requirements.txt -q 
-# pip install -r requirements/requirements-dev.txt -q 
+pip install -r requirements/requirements-test.txt -q 
 pip install -e . -q 
 pytest .
 cd ..
--- scripts/test_server.sh
+++ scripts/test_server.sh
@@ -3,6 +3,6 @@ cd ./m365server
 pip install azure-core
 pip install azure-storage-blob
 pip install -r requirements/requirements.txt -q 
-pip install -r requirements/requirements-dev.txt -q 
+pip install -r requirements/requirements-test.txt -q 
 pip install -e . -q
 pytest .
