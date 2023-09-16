537b0509088f5829dbd59b653547aa1063d5a50b
jphillips <josh.phillips@fearnworks.com>
2023-09-16 06:43:33 -0500

    Update setup to include docker network and pip upg

--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -1,5 +1,8 @@
 python3 -m venv venv 
 source ./venv/bin/activate
+python3 -m pip install -e pip --upgrade 
 pip install -e ./m365server --upgrade
 pip install -e ./m365client --upgrade
 bash ./scripts/run_tests.sh
+docker network create hq-network
+docker compose up --build 
