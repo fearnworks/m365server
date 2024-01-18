6ecbce83eb32549ef4b875c72f5c4d45bf85f8d8
jphillips <josh.phillips@fearnworks.com>
2024-01-18 14:58:26 +0000

    Update build chain

--- scripts/setup_dev.sh
+++ scripts/setup_dev.sh
@@ -4,7 +4,8 @@ fi
 
 source ./venv/bin/activate
 python3 -m pip install pip --upgrade 
-pip install -e ./m365server --upgrade
+pip install pip-tools --upgrade
+pip install -e ./m365server[dev,test] --upgrade
 pip install -e ./m365client --upgrade
 bash ./scripts/run_tests.sh
 
@@ -12,4 +13,4 @@ if [ -z "$(docker network ls | grep hq-network)" ]; then
   docker network create hq-network
 fi
 
-docker compose up --build 
+docker compose up 
