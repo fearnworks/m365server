bd7013bfcc911c218a08e74532728c16738cc186
jphillips <josh.phillips@fearnworks.com>
2024-03-17 07:38:31 -0500

    Resolve dockerfile location

--- m365server/run.sh
+++ m365server/run.sh
@@ -1,12 +1,15 @@
 # check if environment is PRODUCTION or DEVELOPMENT
 if [ "$ENVIRONMENT" = "PRODUCTION" ]; then
-  echo "Running Production Server"
   python3 -m pip install -e .
+  echo "Running Production Server"
+  echo "Running on port $M365_SERVER_PORT"
   uvicorn m365server.main:app --host 0.0.0.0 --port $M365_SERVER_PORT 
 
 else
-    echo "Running Development Server"
     cd /code 
     python3 -m pip install -e .
+    echo "Running Development Server"
+    echo "Running on port $M365_SERVER_PORT"
+
     uvicorn m365server.main:app --host 0.0.0.0 --port $M365_SERVER_PORT --reload
 fi
